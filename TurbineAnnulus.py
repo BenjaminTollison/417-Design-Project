0c5a5ff0f68a26b7627fb1d686d7c4e0b2c705f0
Benjamin Tollison <benjamin.tollison@outlook.com>
2024-12-09 00:17:21 -0600

    found the areas

--- TurbineAnnulus.py
+++ TurbineAnnulus.py
@@ -1,9 +1,11 @@
 import numpy as np
+from DesignConstants import maximum_cycle_temperature
 from AnnulusDimension import annulus_dimensions
 from DesignConstants import (
     mass_flow_rate,
     turbine_efficiency,
     turbine_power,
+    turbine_pressure_ratio,
     maximum_cycle_temperature,
     rotational_speed,
     combustion_chamber_pressure_loss,
@@ -11,8 +13,10 @@ from DesignConstants import (
     inlet_stagnation_pressure,
 )
 from MeanTurbineAngles import (
+    stage_temperature_change,
     flow_coefficient,
     alpha_2,
+    alpha_3,
     specific_heat_gas_pressure,
     specific_heat_ratio_gas,
 )
@@ -41,7 +45,26 @@ density_station_2 = pressure_station_2 / (287 * temperature_station_2)
 area_station_2 = mass_flow_rate / (density_station_2 * axial_velocity_station_2)
 throat_area_station_2 = area_station_2 * np.cos(alpha_2)
 
+axial_velocity_station_3 = axial_velocity_station_2 / np.cos(alpha_3)
+axial_velocity_station_1 = axial_velocity_station_3
+
+temperature_station_1 = maximum_cycle_temperature - axial_velocity_station_1**2 / (2*specific_heat_gas_pressure)
+stagnation_pressure_station_1 = (
+    inlet_stagnation_pressure
+    * compressor_pressure_ratio
+    * (1 - combustion_chamber_pressure_loss)
+)
+pressure_station_1 = stagnation_pressure_station_1*(temperature_station_1/maximum_cycle_temperature)**(specific_heat_ratio_gas/(specific_heat_ratio_gas-1))
+density_station_1 = pressure_station_1 / (287 * temperature_station_1)
+area_station_1 = mass_flow_rate / (density_station_1 * axial_velocity_station_1)
+stagnation_temperature_station_3 = maximum_cycle_temperature - stage_temperature_change
+temperature_station_3 = stagnation_temperature_station_3 - axial_velocity_station_3**2 / (2*specific_heat_gas_pressure)
+stagnation_pressure_station_3 = stagnation_pressure_station_1*turbine_pressure_ratio**-1
+pressure_station_3 = stagnation_pressure_station_3*(temperature_station_3/maximum_cycle_temperature)**(specific_heat_ratio_gas/(specific_heat_ratio_gas-1))
+density_station_3 = pressure_station_3 / (287 * temperature_station_3)
+area_station_3 = mass_flow_rate / (density_station_3 * axial_velocity_station_3)
+
 if __name__ == "__main__":
-    print(density_station_2)
+    print(area_station_1)
     print(area_station_2)
-    print(throat_area_station_2)
+    print(area_station_3)
